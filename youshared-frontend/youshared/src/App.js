5950523c9ec0e6cbdf5e96b721a8c5becf59e09a
nathangoncalvesleon <nathangworks@gmail.com>
2022-01-06 17:39:59 -0300

    atualizado

--- youshared-frontend/youshared/src/App.js
+++ youshared-frontend/youshared/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import { Routes, Route, useNavigate} from 'react-router-dom'
+import Home from './container/Home'
+import Login from './components/Login'
 const App = () => {
     return (
-        <div>
-            
-        </div>
+       <Routes>
+           <Route path="login" element = {<Login/>}/>
+           <Route path="/*" element = {<Home/>}/>
+       </Routes>
     )
 }
 
